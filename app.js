ae36a8a4431aec29ca1e650c5365faed6eaa801b
jno3 <joaoribeiro3891@gmail.com>
2022-08-23 10:42:11 -0300

    model, routes and controllers created, lack implementation

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/conn')
+require('dotenv').config()
+
+const PORT = 3000;
+
+app.use(express.json())
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`server is listening on port ${PORT}`));
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start()
